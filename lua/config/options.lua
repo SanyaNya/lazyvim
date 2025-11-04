b8292a83b21e096a270f5d4e071599b933d1d4c3
SanyaNya <sanyanya4ever@gmail.com>
2025-11-04 13:14:07 +0300

    disable relativenumber globally

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt_local.relativenumber = false
+vim.opt_global.relativenumber = false
 vim.g.autoformat = false
 vim.g.snacks_animate = false
